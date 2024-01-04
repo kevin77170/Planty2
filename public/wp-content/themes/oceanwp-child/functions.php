97000af566b6090f268681d634d388825e08a076
Kevin77170 <kevlan77@live.fr>
2024-01-04 13:10:56 +0100

    modif css

--- public/wp-content/themes/oceanwp-child/functions.php
+++ public/wp-content/themes/oceanwp-child/functions.php
@@ -24,3 +24,13 @@ endif;
 add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 
 // END ENQUEUE PARENT ACTION
+
+// Fonction pour ajouter le lien "Admin" dans le menu
+add_filter( 'wp_nav_menu_items','add_admin_link', 10, 2 );
+function add_admin_link( $items, $args ) {
+    if (is_user_logged_in() && $args->theme_location == 'menu-1') {
+        $items .= '<li><a href="'. get_admin_url() .'">Admin</a></li>';
+    }
+    return $items;
+}
+?>
